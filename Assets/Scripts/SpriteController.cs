e9476055805c1971524ae81120ce39648ee53ee9
Chinmay Chandak <chandakchinmay@gmail.com>
2019-11-15 00:27:50 +0530

    initial commit

---
+++ Assets/Scripts/SpriteController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SpriteController : MonoBehaviour
+{
+    public MainLevelController mlc;
+    public bool amICorrectOption = false;
+
+    private void OnMouseDown()
+    {
+        Debug.Log("i am pressed");
+
+        if (amICorrectOption)
+        {
+            mlc.CorrectOption();
+        }
+        else
+        {
+            mlc.IncorrectOption();
+        }
+    }
+}
