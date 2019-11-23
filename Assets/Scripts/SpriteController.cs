dda82ddb448784b833189bd0ad0bce1325db7470
Chinmay Chandak <chandakchinmay@gmail.com>
2019-11-24 01:53:42 +0530

    removed god mode for screenshots

--- Assets/Scripts/SpriteController.cs
+++ Assets/Scripts/SpriteController.cs
@@ -22,7 +22,7 @@ public class SpriteController : MonoBehaviour
         }
         else
         {
-            gameLogic.CorrectOption();
+            gameLogic.IncorrectOption();
         }
     }
 }
